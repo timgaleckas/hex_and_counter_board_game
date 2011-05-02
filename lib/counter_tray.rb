44d0eef11db860138065854aa1a4f42e901f19cc
Tim Galeckas <tim@galeckas.com>
2011-05-02 15:06:44 -0500

    fix :rolling initial hex state and implement showing counter icons

--- lib/counter_tray.rb
+++ lib/counter_tray.rb
@@ -2,8 +2,12 @@ class CounterTray < Widget
   include ResourceBundle
 
   class CounterSelector < Widget
+    def initialize(*args)
+      @counter_type=args.pop
+      super(*args)
+    end
     def clipped_draw
-      ResourceBundle.counter_tiles[0][0].draw(x,y,z+1)
+      ResourceBundle.counter_tiles[@counter_type][0].draw(x,y,z+1)
     end
   end
   def initialize(x,y,z,width,height,window)
@@ -11,7 +15,7 @@ class CounterTray < Widget
     @counter_selectors = []
     counter_x = counter_y = 0
     ResourceBundle.counter_tiles.each_with_index do |counter_tile, index|
-      @counter_selectors << CounterSelector.new(x+counter_x+10,y+counter_y+10,z,COUNTER_WIDTH+10,COUNTER_HEIGHT+10,window)
+      @counter_selectors << CounterSelector.new(x+counter_x+10,y+counter_y+10,z,COUNTER_WIDTH+10,COUNTER_HEIGHT+10,window,index)
       counter_x += 20 + COUNTER_WIDTH
       if counter_x + 20 + COUNTER_WIDTH > width
         counter_x = 0
--- lib/hex_space.rb
+++ lib/hex_space.rb
@@ -1,6 +1,7 @@
 class HexSpace
   class<<self
     attr_accessor :initial_hex_state
+    attr_accessor :current_rolling_value
   end
   attr_reader :board, :x, :y, :z
 
@@ -11,10 +12,9 @@ class HexSpace
   end
 
   def initial_state
-    return self.class.initial_hex_state
-    # (self.class.initial_hex_state == :rolling) ?
-    #   row*@columns+column :
-    #   @initial_hex_state
+    return self.class.initial_hex_state == :rolling ?
+      self.class.current_rolling_value = ((self.class.current_rolling_value || -1) + 1) :
+      self.class.initial_hex_state
   end
 
   def draw(x_offset,y_offset)
